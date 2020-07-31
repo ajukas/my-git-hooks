08b391c1835fce087f28c438b34ab52913aa4574
Mikail Ajukas <majukas@uolinc.com>
2020-07-31 16:12:46 -0300

    Setting chmod on hook

--- install_hooks.sh
+++ install_hooks.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-wget https://raw.githubusercontent.com/ajukas/my-git-hooks/master/prepare-commit-msg && mv prepare-commit-msg .git/hooks/prepare-commit-msg
+wget https://raw.githubusercontent.com/ajukas/my-git-hooks/master/prepare-commit-msg && chmod +x prepare-commit-msg && mv prepare-commit-msg .git/hooks/prepare-commit-msg
